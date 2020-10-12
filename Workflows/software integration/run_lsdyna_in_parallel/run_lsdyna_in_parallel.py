4632b43a58011772999f70650f941110c1f9d276
Martin <martin@entail.no>
2020-10-12 06:49:20 +0200

    Udpate

--- Workflows/software integration/run_lsdyna_in_parallel/run_lsdyna_in_parallel.py
+++ Workflows/software integration/run_lsdyna_in_parallel/run_lsdyna_in_parallel.py
@@ -6,10 +6,12 @@ from files.functions import populate_runfiles, rundyna, did_I_hit, makeplot
 
 
 vel = 3.1 # Velocity in m/s
-angles = np.linspace(51,70,3); endtime = 2.00
+# angles = np.linspace(51,70,3); endtime = 2.00
 # angles = [50]; endtime = 0.02
 
-# 24, 25,
+angles = [24, 25, 57, 58, 59]; endtime = 2.02
+
+
 
 wf_inputs = {
         'wf_run_name' : 'Branch duplicate LS-DYNA',
